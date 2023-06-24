3187efb108ef2866dc5642310de3a668b29e9312
kolyasik <o687801077@gmail.com>
2023-06-24 12:23:39 +0300

    changed path of static images in aboutUsInfoItems

--- src/app/Static Data/AboutUs/AboutUsInfoItems.js
+++ src/app/Static Data/AboutUs/AboutUsInfoItems.js
@@ -3,7 +3,7 @@ const InfoItemsAboutUs = [
         id: 1,
         title: 'Winner coaches',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        src: '/../public/media/images/community.png'
+        src: '@/assets/community.png'
     },
     {
         id: 2,

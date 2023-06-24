c2c53bc774a52c77b8e0f13e25143e52539a96c5
kolyasik <o687801077@gmail.com>
2023-06-24 14:19:23 +0300

    add cl

--- src/pages/api/user/login.js
+++ src/pages/api/user/login.js
@@ -10,6 +10,7 @@ async function handler(req, res) {
             const {email, auth} = req.body;
             const userData = await userService.login(email, auth);
             const cookies = new Cookies(req, res);
+            console.log('cookies & userDAta', cookies, userData);
             cookies.set('refreshToken', userData.refreshToken, {
                 maxAge: 30*24*60*60*1000,
                 httpOnly: true

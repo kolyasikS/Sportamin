d366ace580e9b16e4dcc6f1cb8472e1844f5c9ac
kolyasik <o687801077@gmail.com>
2023-06-25 18:23:29 +0300

    fix bugs

--- src/widgets/Header/Header.jsx
+++ src/widgets/Header/Header.jsx
@@ -2,12 +2,11 @@ import React, {useEffect, useState} from 'react';
 import styles from './styles/Header.module.scss';
 import {Loading, MainLogo} from "@/shared/ui/Logos/api/Logos";
 import {RBButton} from "@/shared/ui/Buttons/api/Buttons";
-import {useDispatch, useSelector} from "react-redux";
-import {authenticate, registration} from "@/app/lib/controllers/authController";
+import {useSelector} from "react-redux";
+import {authenticate} from "@/app/lib/controllers/authController";
 import {useRouter} from "next/router";
 import ProfileMenu from "@/widgets/Header/ProfileMenu";
 import NavMenu from "@/widgets/Header/NavMenu";
-import defaultAvatar from "@assets/profile/default_avatar.png";
 const Header = ({isLoading, avatar}) => {
     const isAuthState = useSelector(state => state.authReducer.isAuth);
     const isFetching = useSelector(state => state.sessionReducer.isLoading);
@@ -17,19 +16,13 @@ const Header = ({isLoading, avatar}) => {
         setIsAuth(isAuthState);
     }, [isAuthState]);
 
-    const dispatch = useDispatch();
 
     return (
         <div className={styles.introHeader}>
             {isFetching && <div className={styles.fetching}></div>}
             <MainLogo/>
             <NavMenu/>
-            <div className={styles.profile} onClick={() => {
-                registration(dispatch, {
-                    email: 'mykola.primachenko@gmail.com',
-                    password: 'optimus33',
-                })
-            }}>
+            <div className={styles.profile}>
                 {isLoading || (isAuth && !avatar)
                     ? <Loading/>
                     : isAuth
